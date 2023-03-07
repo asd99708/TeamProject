96bc160c234a36cc33f4adeb0806eb958ec55498
Administrator <asd99708@naver.com>
2023-03-07 10:08:13 +0900

    초기 설정

---
+++ src/main/java/com/wedogo/WedogoApplication.java
@@ -0,0 +1,13 @@
+package com.wedogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedogo/WedogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
