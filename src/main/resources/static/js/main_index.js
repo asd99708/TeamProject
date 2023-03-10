f5cdef69715cce32b238c33510b301cbf6e588b7
Administrator <asd99708@naver.com>
2023-03-10 17:57:10 +0900

    2023-03-10 수정본

---
+++ src/main/resources/static/js/main_index.js
@@ -0,0 +1,9 @@
+<script>
+        $(function() {
+            $("#start-date").datepicker({
+            dateFormat: "yy-mm-dd"
+            });
+            $("#end-date").datepicker({
+            dateFormat: "yy-mm-dd"
+    });
+        </script>
