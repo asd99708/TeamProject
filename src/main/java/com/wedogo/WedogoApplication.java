4c0c024ebf54549c29b1edc3d07f8f54dd0b2be9
Administrator <asd99708@naver.com>
2023-03-10 12:52:50 +0900

    2023-03-10 수정본

--- src/main/java/com/wedogo/WedogoApplication.java
+++ src/main/java/com/wedogo/WedogoApplication.java
@@ -2,9 +2,10 @@ package com.wedogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+
+
 @SpringBootApplication
 public class WedogoApplication {
 
--- src/main/java/com/wedogo/controller/MainController.java
+++ src/main/java/com/wedogo/controller/MainController.java
@@ -1,14 +1,25 @@
 package com.wedogo.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/main")
+@RequestMapping("/")
 public class MainController {
-    @GetMapping(value ="/index")
-    public String index(){
-        return "index";
+    @GetMapping
+    public String home() {
+        return "main/index.html";
+    }
+
+    @GetMapping("/registerhome")
+    public String registerhome() {
+        return "register/registerhome.html";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register/register.html";
     }
 }
