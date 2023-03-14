359bac86c85dbed0f089c3bcca51caf221403b12
Administrator <asd99708@naver.com>
2023-03-14 19:50:10 +0900

    db연동과정

--- src/main/java/com/wedogo/dto/UserInfoDTO.java
+++ src/main/java/com/wedogo/dto/UserInfoDTO.java
@@ -1,19 +1,33 @@
 package com.wedogo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@ToString
+@NoArgsConstructor
 public class UserInfoDTO {
-    Long idnum;
+    @NotBlank(message = "아이디를 입력해주세요.")
     String id;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     String passward;
+
+    @NotBlank(message = "이름을 입력해주세요.")
     String username;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
     String nickname;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     String email;
+
     String profileImage;
     int rate;
 }
