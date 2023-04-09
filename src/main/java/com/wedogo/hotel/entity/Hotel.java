0dd0d078da4ca0fd6fad9da3dda40a2a0745ab6f
Lee <asd99708@naver.com>
2023-04-10 01:31:58 +0900

    2023-04-10

--- src/main/java/com/wedogo/hotel/entity/Hotel.java
+++ src/main/java/com/wedogo/hotel/entity/Hotel.java
@@ -1,30 +1,36 @@
 package com.wedogo.hotel.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.wedogo.board.entity.Userinfo;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@Getter
-@Setter
 @Table(name = "hotel")
 public class Hotel {
+
     @Id
     @Column(name = "hotel_name")
     private String hotelName;
 
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false)
     private String userId;
 
-    @Column(name = "room_max")
+    @Column(name = "room_max", nullable = false)
     private int roomMax;
 
-    @Column(name = "room_content")
+    @Column(name = "room_content", nullable = false)
     private String roomContent;
 
-    @Column(name = "room_image1")
+    @Column(name = "room_image1", nullable = false)
     private String roomImage1;
 
-    // Add getters and setters for each field
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", insertable = false, updatable = false)
+    private Userinfo userinfo;
+
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Room> rooms;
+
+    // Add constructors, getters, and setters for each field
 }
