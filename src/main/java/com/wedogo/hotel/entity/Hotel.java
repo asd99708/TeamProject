63d7154b478cb4795ab3c5c091f0f5189bf7710e
asd99708 <asd99708@naver.com>
2023-04-10 12:56:37 +0900

    2023-04-10

--- src/main/java/com/wedogo/hotel/entity/Hotel.java
+++ src/main/java/com/wedogo/hotel/entity/Hotel.java
@@ -1,11 +1,15 @@
 package com.wedogo.hotel.entity;
 
 import com.wedogo.board.entity.Userinfo;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "hotel")
 public class Hotel {
 
@@ -13,24 +17,24 @@ public class Hotel {
     @Column(name = "hotel_name")
     private String hotelName;
 
-    @Column(name = "user_id", nullable = false)
+    @Column(name = "user_id")
     private String userId;
 
-    @Column(name = "room_max", nullable = false)
+    @Column(name = "room_max")
     private int roomMax;
 
-    @Column(name = "room_content", nullable = false)
+    @Column(name = "room_content")
     private String roomContent;
 
-    @Column(name = "room_image1", nullable = false)
+    @Column(name = "room_image1")
     private String roomImage1;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "user_id", insertable = false, updatable = false)
-    private Userinfo userinfo;
-
-    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Room> rooms;
+//    @ManyToOne
+//    @JoinColumn(name = "user_id", referencedColumnName = "user_id", insertable = false, updatable = false)
+//    private Userinfo userinfo;
+//
+//    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    private List<Room> rooms;
 
     // Add constructors, getters, and setters for each field
 }
